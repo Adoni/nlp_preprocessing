2c730d8f864578e999894039a409ce1154304cc0
Sun Xiaofei <adoni1203@gmail.com>
2018-06-02 15:01:57 +0800

    Add new folder

---
+++ preprocessing/__init__.py
@@ -0,0 +1,12 @@
+# encoding: utf-8
+"""
+    __init__.py.py
+    ~~~~~~~~~
+    
+    Model description
+    
+    :author: Xiaofei Sun
+    :contact: adoni1203@gmail.com
+    :date: 2018/6/2
+    :license: MIT, see LICENSE for more details.
+"""
