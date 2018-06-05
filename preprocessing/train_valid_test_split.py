db8ed22b0299e44ec8bc4b8b4331303ce7034506
Sun Xiaofei <adoni1203@gmail.com>
2018-06-05 14:26:07 +0800

    <fix> save order file

--- preprocessing/train_valid_test_split.py
+++ preprocessing/train_valid_test_split.py
@@ -22,7 +22,8 @@ import numpy
 parser = argparse.ArgumentParser(description='Shuffle dataset into train, valid and test')
 parser.add_argument('--input', type=str, help='path of corpus file')
 parser.add_argument('--output', type=str, help='path of output file')
-parser.add_argument('--order', type=str, default='', help='path of order, use random order if it is empty')
+parser.add_argument('--save-order-to', type=str, default='', help='path of order')
+parser.add_argument('--load-order-from', type=str, default='', help='path of order, use random order if it is empty')
 args = parser.parse_args()
 
 sentences = []
@@ -33,11 +34,11 @@ with open(args.input, "r", encoding="utf-8") as f:
             continue
         sentences.append(line)
 
-if args.order == '':
+if args.load_order_from == '':
     order = list(range(len(sentences)))
     numpy.random.shuffle(order)
 else:
-    with open(args.order, "r", encoding="utf-8") as f:
+    with open(args.load_order_from, "r", encoding="utf-8") as f:
         order = [int(line.strip()) for line in f]
 
 sentences = [sentences[i] for i in order]
@@ -55,3 +56,9 @@ with open(os.path.join(args.output, "valid.txt"), "w", encoding="utf-8") as f:
     f.write('\n'.join(sentences[train_count:train_count+valid_count]))
 with open(os.path.join(args.output, "test.txt"), "w", encoding="utf-8") as f:
     f.write('\n'.join(sentences[train_count+valid_count:]))
+
+if args.save_order_to != '':
+    order = [str(i) for i in order]
+    with open(args.save_order_to, "w", encoding="utf-8") as f:
+        f.write('\n'.join(order))
+
